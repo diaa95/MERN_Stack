e30e69dd39f64ac7548939556702c27deafed8cd
Diaa AbdAlDayem <Diaa.abdaldayem@gmail.com>
2021-02-02 14:45:30 +0200

    axios

--- pokemon_api/src/components/PokemonApi.js
+++ pokemon_api/src/components/PokemonApi.js
@@ -1,14 +1,16 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 
 const PokemonApi = (props) =>{
     const [pokemons, setPokemons] = useState([]);
 
     const handleClick = (e) =>{
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
-            .then(response => {
-                return response.json();
-            }).then(response => {
-            setPokemons(response.results.map(item => item.name));
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=807").then(response => {
+            return response;
+            console.log(response);
+        }).then(response => {
+            setPokemons(response.data.results.map(item => item.name));
+            console.log(pokemons);
         }).catch(err=>{
             console.log(err);
         });
